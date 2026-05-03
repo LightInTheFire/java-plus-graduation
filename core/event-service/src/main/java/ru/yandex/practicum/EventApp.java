c484010e181d5ec40b0f496d32cc18bb0e056ceb
Ilia Egorov <aleilmessia@mail.ru>
2026-05-03 18:14:55 +0300

    feat: rename main service to event service

--- core/event-service/src/main/java/ru/yandex/practicum/EventApp.java
+++ core/event-service/src/main/java/ru/yandex/practicum/EventApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EwmApp {
+public class EventApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(EwmApp.class, args);
+        SpringApplication.run(EventApp.class, args);
     }
 }
