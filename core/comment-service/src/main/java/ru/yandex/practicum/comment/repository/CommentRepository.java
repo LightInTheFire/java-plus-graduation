9a60e4b22fd57799257193362e801aaac257cb0b
Ilia Egorov <aleilmessia@mail.ru>
2026-05-04 19:51:58 +0300

    fix: remove entitygraph

--- core/comment-service/src/main/java/ru/yandex/practicum/comment/repository/CommentRepository.java
+++ core/comment-service/src/main/java/ru/yandex/practicum/comment/repository/CommentRepository.java
@@ -6,14 +6,12 @@ import ru.yandex.practicum.comment.model.Comment;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @EntityGraph(attributePaths = "author")
     Page<Comment> findAllByEventId(Long eventId, Pageable pageable);
 
     List<Comment> findAllByAuthorId(Long authorId, Pageable pageable);
